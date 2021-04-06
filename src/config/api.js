78845e7f991825cf520aed8954c3df8517575fd0
iden <dennydharmawan717@gmail.com>
2021-04-06 22:55:52 +0700

    edit env config api

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: "http://localhost:5000/api/v1",
+  baseURL: process.env.URL + process.env.API_VERSION,
 });
 
 export const setAuthToken = (token) => {
